0a439fa90e0dbe78964c0840a88cfa27eb61f785
Hare Krishna <shivanshu singh>
2024-01-27 14:49:43 +0530

    Hello World

---
+++ Hello-World/index.rs
@@ -0,0 +1,8 @@
+// Rust use .rs as extension
+
+// The main function is the entry point of every Rust program
+fn main() {
+    // println! is a macro in Rust used for printing text to the console
+    // "Hello World this is ShellBng!!!" is the text to be printed
+    println!("Hello World this is ShellBng!!!");
+}
