b78315383155555886bb94b6c4a781ea6e1cc4a4
Hare Krishna <shivanshu singh>
2024-02-05 16:48:31 +0530

    Done with compare

--- CompareNo/src/main.rs
+++ CompareNo/src/main.rs
@@ -1,20 +1,9 @@
-fn main() {
-    let number = 42;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-    match number {
-        0 => print!("It's zero!"),
-        _ => print!("it is something else")
-                
-    }
+fn main(){
+    println!("Enter the number ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
 }
-// fn main() {
-//     let number = 42;
-
-//     match number {
-//         0 => println!("It's zero!"),
-//         1 | 2 => println!("It's one or two!"),
-//         3..=10 => println!("It's between 3 and 10 (inclusive)!"),
-//         n if n % 2 == 0 => println!("It's an even number!"),
-//         _ => println!("It's something else!"),
-//     }
-// }
---
+++ CompareNo/target/debug/deps/rand_chacha-01e59ee97a8c5485.d
@@ -0,0 +1,7 @@
+D:\Projects\RUST\CompareNo\target\debug\deps\librand_chacha-01e59ee97a8c5485.rmeta: C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+D:\Projects\RUST\CompareNo\target\debug\deps\rand_chacha-01e59ee97a8c5485.d: C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\shiva\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
